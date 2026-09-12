635189bf34e4e7c050cd3a1885c3467ae4d87326
Ruslan Balatskyi <ruslanttt281@gmail.com>
2026-09-12 14:22:36 +0200

    Allow Vercel frontend in CORS

--- Program.cs
+++ Program.cs
@@ -24,7 +24,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:3000",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://freegram-frontend.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
